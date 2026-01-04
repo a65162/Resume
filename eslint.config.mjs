3670276793f7de8e45f1bf0b9416aa5eeffb95ca
Wei <git@a-wei.link>
2026-01-04 16:31:05 +0800

    build(eslint): add react hook preset and react preset

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import ReactHooks from 'eslint-plugin-react-hooks'
+import ReactEslint from 'eslint-plugin-react'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ReactEslint.configs.flat.recommended,
+  ReactHooks.configs.flat.recommended,
+  ReactEslint.configs.flat["jsx-runtime"],
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
